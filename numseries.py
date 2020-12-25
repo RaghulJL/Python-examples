a7c9a9a4f78bdbd158e70fa5f14d5e33e8792500
RaghulJL <76468264+RaghulJL@users.noreply.github.com>
2020-12-25 11:09:58 +0530

    Update numseries.py

--- numseries.py
+++ numseries.py
@@ -1,23 +1,12 @@
 num = int(input('enter the number: '))
-
 a=0
-
 b=0
-
 for i in range(1,num+1):
-
     if(i%2!=0):
-
         a= a+7
-
     else:
-
         b = b+6
-
 if(num%2!=0):
-
     print(' {} term of series is {}')
-
 else:
-
     print('{} term of series is {}')
